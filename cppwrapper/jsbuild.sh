044077820d3be98f775f192997f1946dbb582ea4
Division <pmujik@gmail.com>
2018-01-18 11:19:45 +0100

    build script updated

--- cppwrapper/jsbuild.sh
+++ cppwrapper/jsbuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # if emmake command not found try running
-# > source /Users/nsidorenko/Documents/projects/emsdk-portable/emsdk_env.sh
+# > source ~/Documents/projects/emsdk-portable/emsdk_env.sh
 # Change path to correct path to the emsdk folder
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -9,4 +9,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR}
 mkdir -p build_js
 cd build_js
-emcmake cmake .. && make
+
+if ! type "emcmake" > /dev/null; then
+  echo "emcmake command not found"
+  echo "It can be fixed by executing command:"
+  echo "> source <path_to_emsk_folder>/emsdk_env.sh"
+else
+  emcmake cmake .. && make
+fi
