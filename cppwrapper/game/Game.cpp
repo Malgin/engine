1f5db0194e9cadc124f67e04c3b7a94ebebee340
Division <pmujik@gmail.com>
2018-05-18 16:45:00 +0300

    Basic terrain render

--- cppwrapper/game/Game.cpp
+++ cppwrapper/game/Game.cpp
@@ -4,6 +4,7 @@
 
 #include "Game.h"
 #include "objects/Sprite.h"
+#include "objects/Terrain.h"
 #include "loader/HierarchyLoader.h"
 #include <vector>
 #include "EngMath.h"
@@ -17,6 +18,8 @@ std::shared_ptr<Sprite> sprite2;
 std::shared_ptr<Sprite> sprite3;
 CameraPtr camera;
 
+TerrainPtr terrain;
+
 LightObjectPtr light;
 LightObjectPtr light2;
 GameObjectPtr lightRing1;
@@ -29,31 +32,46 @@ ModelBundlePtr bundle;
 void Game::init(Engine *engine) {
   _engine = engine;
 
+  terrain = CreateGameObject<Terrain>();
+  terrain->loadHeightmap("resources/terrain/terrain.raw");
+//  terrain->addTextures("resources/terrain/snow2_d.jpg", "resources/terrain/normal_test.jpg");
+
+  terrain->addTextures("resources/terrain/snow_mud_d.jpg", "resources/terrain/snow_mud_n.jpg");
+  terrain->addTextures("resources/terrain/snow2_d.jpg", "resources/terrain/snow2_n.jpg");
+  terrain->addTextures("resources/terrain/snow_mntn2_d.jpg", "resources/terrain/snow_mntn2_n.jpg");
+  terrain->loadSplatmap("resources/terrain/splatmap.png");
+  terrain->loadSpecularmap("resources/terrain/specular.jpg");
+  terrain->transform()->position(vec3(-15,-2,-15));
+
   bundle = Resources::loadModel("resources/models/group.mdl");
   rootObj = loader::loadHierarchy(bundle);
+  rootObj->transform()->position(vec3(0, 3, 0));
 
   light = CreateGameObject<LightObject>();
   light->transform()->position(vec3(0, 4, 0));
-  light->radius(20);
-  light->color(vec3(0, 1, 0));
+  light->radius(14);
+  light->color(vec3(1, 1, 1));
   light->enableDebug();
 
-  light2 = CreateGameObject<LightObject>();
-  light2->transform()->position(vec3(0, 4, 0));
-  light2->radius(30);
-  light2->color(vec3(1, 0, 0));
-  light2->enableDebug();
+//  light2 = CreateGameObject<LightObject>();
+//  light2->transform()->position(vec3(0, 4, 0));
+//  light2->radius(30);
+//  light2->color(vec3(1, 0, 0));
+//  light2->enableDebug();
 
   lightRing1 = CreateGameObject<GameObject>();
   lightRing1->transform()->rotation(glm::angleAxis((float)M_PI / 2, vec3(0, 1, 0)));
-  lightRing1->transform()->position(vec3(1.2, 0, 1));
-  int ringCount = 10;
+  lightRing1->transform()->position(vec3(1.2, 1, 1));
+  int ringCount = 2;
+//  int ringCount = 0;
   for (int i = 0; i < ringCount; i++) {
     auto lightInRing = CreateGameObject<LightObject>();
-    float ang = M_PI * 2 * i / 10.0f;
-    lightInRing->transform()->position(vec3(cosf(ang) * 4, 0, sinf(ang) * 4));
-    lightInRing->radius(7);
-    lightInRing->color(vec3(0, 1, 1));
+    float ang = M_PI * 2 * i / ringCount;
+    lightInRing->transform()->position(vec3(cosf(ang) * 7, 0, sinf(ang) * 7));
+//    lightInRing->radius(7);
+//    lightInRing->color(i % 2 == 0 ? vec3(0, 1, 1) : vec3(0.8, 0.2, 0.5));
+    lightInRing->radius(45);
+    lightInRing->color(vec3(1,1,1));
     lightInRing->enableDebug();
     lightInRing->transform()->parent(lightRing1->transform());
   }
@@ -124,7 +142,9 @@ void Game::_updateInput(float dt) {
   }
 
   if (input->keyDown(Key::Space)) {
-    rootObj->transform()->rotate(vec3(0, 0, 1), dt * PI);
+
+    light->transform()->position(camera->transform()->position() + camera->transform()->forward() * 2.5f);
+//    rootObj->transform()->rotate(vec3(0, 0, 1), dt * PI);
   }
 
   if (input->keyDown(Key::MouseLeft)) {
@@ -136,7 +156,7 @@ void Game::_updateInput(float dt) {
 }
 
 void Game::_updateGameLogic(float dt) {
-  dt *= 0.05;
+//  dt *= 0.05;
   ang += dt * PI;
 
   quat rotation(vec3(camXAngle, camYAngle, 0));
@@ -146,6 +166,6 @@ void Game::_updateGameLogic(float dt) {
   sprite1->transform()->rotate(vec3(0, 0, 1), dt * PI);
   sprite2->transform()->rotate(vec3(0, 0, 1), dt * PI * 2);
 
-  light->transform()->setPosition(vec3(cos(ang) * 5, 3, sin(ang) * 5));
+//  light->transform()->setPosition(vec3(cos(ang) * 9, 3, sin(ang) * 9));
   lightRing1->transform()->rotate(vec3(0, 1, 0), dt * PI * 0.2);
 }
---
+++ resources/shaderTpl/lighting.tpl
@@ -0,0 +1,90 @@
+{% if MODE == "FRAGMENT_MAIN" %}
+{% if not SPECULAR_MAP %}
+  float materialSpecular = 1.0;
+{% endif %}
+
+  float TILE_SIZE = 32.0;
+  vec2 screenSize = vec2(camera.screenSize);
+  ivec2 tilesCount = ivec2(ceil(screenSize / TILE_SIZE));
+
+  int tileX = int(floor(gl_FragCoord.x / TILE_SIZE));
+  int tileY = int(floor(gl_FragCoord.y / TILE_SIZE));
+  vec2 tileCount = ceil(vec2(screenSize / TILE_SIZE));
+
+{% if USE_BUFFER_TEXTURE %}
+  int tileIndex = tileX + tilesCount.x * tileY;
+  uvec4 gridItem = texelFetch(uLightGrid, tileIndex);
+{% else %}
+  uvec4 gridItem = texture(uLightGrid, vec2(float(tileX) / tileCount.x, float(tileY) / tileCount.y));
+{% endif %}
+
+  uint lightOffset = gridItem.r;
+  uint lightCount = gridItem.g;
+  vec3 eyeDir_worldspace = normalize(camera.position - vPosition_worldspace.xyz); // vector to camera
+  vec4 lightsColor = vec4(0, 0, 0, 1);
+
+  for (uint i = 0u; i < lightCount; i++) {
+    uint currentOffset = lightOffset + i;
+{% if USE_BUFFER_TEXTURE %}
+    uint lightIndex = texelFetch(uLightIndices, int(currentOffset)).r;
+{% else %}
+    uint lightIndex = texelFetch(uLightIndices, ivec2(currentOffset % 4096u, int(floor(float(currentOffset) / 4096.0))), 0).r;
+{% endif %}
+    vec3 lightPosition = lights[lightIndex].position;
+    vec3 lightDir = vPosition_worldspace.xyz - lightPosition;
+    float distanceToLight = length(lightDir);
+    lightDir /= distanceToLight; // normalize
+    vec3 lightValue = calculateFragmentDiffuse(distanceToLight, lights[lightIndex].attenuation, normal_worldspace, lightDir, eyeDir_worldspace, lights[lightIndex].color, materialSpecular);
+    lightsColor += vec4(lightValue, 0.0);
+  }
+
+  lightsColor += ambient;
+  fragmentColor *= lightsColor + ambient;
+{% endif %}
+
+{% if MODE == "FRAGMENT_UNIFORM_DECLARE" %}
+{% if USE_BUFFER_TEXTURE %}
+uniform usamplerBuffer uLightGrid;
+uniform usamplerBuffer uLightIndices;
+{% else %}
+uniform highp usampler2D uLightGrid;
+uniform highp usampler2D uLightIndices;
+{% endif %}
+
+struct Light {
+  vec3 position;
+  float attenuation;
+  vec3 color;
+};
+
+layout (std140) uniform LightBlock {
+  Light lights[100];
+};
+
+
+struct Camera {
+  vec3 position;
+  uvec2 screenSize;
+};
+
+
+layout (std140) uniform CameraBlock {
+  Camera camera;
+};
+
+in vec3 vNormal_worldspace;
+//in vec3 vEyeDirection_cameraspace;
+vec4 ambient = vec4(0.1, 0.1, 0.1, 0.0) * 0.0;
+
+vec3 calculateFragmentDiffuse(float distanceToLight, float attenuation, vec3 normal, vec3 lightDir, vec3 eyeDir, vec3 lightColor, float materialSpecular) {
+  float lightValue = clamp(dot(-lightDir, normal), 0.0, 1.0);
+  float attenuationValue = 1.0 / (1.0 + attenuation * pow(distanceToLight, 2.0));
+  vec3 diffuse = lightColor * lightValue;
+
+  vec3 reflect = reflect(lightDir, normal);
+  float cosAlpha = clamp(dot(eyeDir, reflect), 0.0, 1.0);
+  vec3 specular = pow(cosAlpha, 32.0) * lightColor * materialSpecular;
+
+  return attenuationValue * (diffuse + specular);
+}
+{% endif %}
